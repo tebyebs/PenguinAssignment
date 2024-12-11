ce8ef137954628edfe2fa044e827988f032c4651
tebyebs <newc6802@ox.ac.uk>
2024-12-11 16:52:03 +0000

    added explore plot

--- functions/plotting.R
+++ functions/plotting.R
@@ -1,4 +1,3 @@
-
 plot_boxplot <- function(data, 
                          x_column, 
                          y_column, 
@@ -32,3 +31,52 @@ plot_boxplot <- function(data,
     theme_bw()
 }
 
+#plot scatterplot
+plot_scatter <- function(data, 
+                         x_axis, 
+                         y_axis, 
+                         x_label, 
+                         y_label,
+                         colour_factor) {
+  
+  # Now make the plot
+  ggplot(data = data, 
+         aes(
+           x =  x_axis, 
+           y =  y_axis, 
+           color =  colour_factor,
+           shape = colour_factor)) +  # Use {{ }} for x and y columns
+    geom_point(
+      size = 2,
+      alpha = 0.8,
+      show.legend = FALSE) +
+    labs(
+      x = x_label, 
+      y = y_label) +  # Use provided x and y labels
+    theme_light() + 
+    theme(legend.position = "bottom")
+}
+
+
+#plots pngs
+save_flipper_plot_png <- function(boxplot, 
+                                  filename, size, res, scaling){
+  agg_png(filename, width   =  size, 
+          height  =  size, 
+          units   =  "cm", 
+          res     =  res, 
+          scaling =  scaling)
+  print(boxplot)
+  dev.off()
+}
+
+#plots svgs
+save_flipper_plot_svg <- function(boxplot, 
+                                  filename, size, scaling){
+  size_inches = size/2.54
+  svglite(filename, width   = size_inches, 
+          height  = size_inches, 
+          scaling = scaling)
+  print(boxplot)
+  dev.off()
+}
