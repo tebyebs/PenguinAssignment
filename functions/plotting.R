77d67bd0f45852c0357bf1ec6ec2ff31b0c7ba87
tebyebs <newc6802@ox.ac.uk>
2024-12-11 17:31:00 +0000

    added saved fig

--- functions/plotting.R
+++ functions/plotting.R
@@ -55,25 +55,30 @@ plot_scatter <- function(data,
 }
 
 
-#plots pngs
-save_flipper_plot_png <- function(boxplot, 
-                                  filename, size, res, scaling){
+#saves pngs
+save_plot_png <- function(plot,
+                          filename,
+                          size,
+                          res,
+                          scaling){
   agg_png(filename, width   =  size, 
           height  =  size, 
           units   =  "cm", 
           res     =  res, 
           scaling =  scaling)
-  print(boxplot)
+  print(plot)
   dev.off()
 }
 
-#plots svgs
-save_flipper_plot_svg <- function(boxplot, 
-                                  filename, size, scaling){
+#saves svgs
+save_plot_svg <- function(plot,
+                          filename,
+                          size,
+                          scaling){
   size_inches = size/2.54
   svglite(filename, width   = size_inches, 
           height  = size_inches, 
           scaling = scaling)
-  print(boxplot)
+  print(plot)
   dev.off()
 }
